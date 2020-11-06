8506e105e1b9bbc598ba1119ff7d52571d3ebb03
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-06 16:29:43 +0700

    PBOIF2-10119082-Latihan34-Kalkulator

---
+++ src/pboif2/pkg10119082/latihan34/kalkulator/Kalkulator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan34.kalkulator;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Kalkulator
+ */
+public class Kalkulator {
+    double value1,value2;
+    
+    public double tambahBilangan(){
+        return value1+value2;
+    }
+    
+    public double kurangBilangan(){
+        return value1-value2;
+    }
+    
+    public double kaliBilangan(){
+        return value1*value2;
+    }
+    
+    public double bagiBilangan(){
+        return value1/value2;
+    }
+    
+    public double sisaBilangan(){
+        return value1%value2;
+    }
+}
